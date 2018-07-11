eb06a0fb18b74e150f6fbdbf3ff25e8fee103ebe
k8Howard <sweetk8@me.com>
2018-07-11 20:35:05 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,11 @@
 #write out your code here
 
 def least_coins(cents)
+  quarters = cents /25
+  dimes = cents % 25 /10
+  nickles = cents % 25 % 10 /5
+  pennies = cents % 25 % 10 % 5 /1
   
-
-end
+  
+  
+  end
