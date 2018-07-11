2e2eebe70d0b69867780b249ac3b1da3dac76e7d
k8Howard <sweetk8@me.com>
2018-07-11 20:37:49 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -5,5 +5,5 @@ def least_coins(cents)
   dimes = cents % 25 /10
   nickles = cents % 25 % 10 /5
   pennies = cents % 25 % 10 % 5 /1
-  hash {:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4}
+  hash {:quarters => quarters, :dimes => dimes, :nickels => nickles, :pennies => pennies}
 end
